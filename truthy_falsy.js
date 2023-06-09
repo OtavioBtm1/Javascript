85a9f725912e10cc96aa864ab1b0c2d0fbe96c25
OtavioBtm1 <rm552012@fiap.com.br>
2023-06-09 16:49:31 -0300

    Adição de novos codigos explicativos

---
+++ truthy_falsy.js
@@ -0,0 +1,15 @@
+///boolean
+const usuariologado = true
+const contapaga = false
+/// truthy e falsy
+// 0=> falsy e 1=> true
+
+console.log(0== false) // o resultaddo será true
+console.log("" == false)// também será true
+console.log(1 == true)// será true pois 1=truthy
+
+//null é um tipo de dado que representa o vazio, diferente de undefined, o null é declarado, Exemplo:
+
+let idade1= null
+let idade2
+console.log(idade1, idade2)// retornará "null undefined" pois a declaração de null é uma declaração vazia, já a undefined é basicamente nada
